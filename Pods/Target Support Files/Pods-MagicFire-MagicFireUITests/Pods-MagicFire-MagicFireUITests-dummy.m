22e9cde62a1948206d7d9543a070ef765d1f0874
SkylineEC <e0724539@u.nus.edu>
2021-09-06 11:31:38 +0800

    First Commit
    hello

---
+++ Pods/Target Support Files/Pods-MagicFire-MagicFireUITests/Pods-MagicFire-MagicFireUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MagicFire_MagicFireUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MagicFire_MagicFireUITests
+@end
---
+++ Pods/Target Support Files/Pods-MagicFireTests/Pods-MagicFireTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MagicFireTests : NSObject
+@end
+@implementation PodsDummy_Pods_MagicFireTests
+@end
