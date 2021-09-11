dc03d49e226857dde5c435ab25e9efd27b7d2598
SkylineEC <e0724539@u.nus.edu>
2021-09-11 17:22:42 +0800

    Update ViewController.swift
    new branch

--- MagicFire/ViewController.swift
+++ MagicFire/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello Firebase")
+        print("This is a new branch")
     }
 
 
